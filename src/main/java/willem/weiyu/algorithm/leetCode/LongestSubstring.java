ead2dca085c2fa3bf72bb7cae081ca9cdc5e5b8f
weiyu005 <weiyu005@ke.com>
2021-01-19 22:54:17 +0800

    tianjia

---
+++ src/main/java/willem/weiyu/algorithm/leetCode/LongestSubstring.java
@@ -0,0 +1,39 @@
+package willem.weiyu.algorithm.leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author weiyu005@ke.com
+ * @Description No3 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+ * @Date 2021/1/19 21:31
+ */
+public class LongestSubstring {
+
+    public static int solution1(String str){
+        if (str.length() < 2) {
+            return str.length();
+        }
+        char[] charArr = str.toCharArray();
+        List<Character> list = new ArrayList<>(charArr.length);
+        int length=0;
+        for (int i = 0; i < charArr.length; i++) {
+            char ch = charArr[i];
+            list.add(ch);
+            Long distinctLength = list.stream().distinct().count();
+            if (list.size() == distinctLength){
+                length = list.size();
+            } else {
+                length = list.size();
+                int index = list.indexOf(ch);
+                List<Character> tempList = list.subList(index +1, list.size());
+                list = tempList;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution1("abcabcbb"));
+    }
+}
---
+++ src/main/java/willem/weiyu/algorithm/leetCode/Median.java
@@ -0,0 +1,38 @@
+package willem.weiyu.algorithm.leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author weiyu005@ke.com
+ * @Description No4 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数
+ * @Date 2021/1/19 22:37
+ */
+public class Median {
+
+    public static float solution(List<Integer> list1,List<Integer> list2){
+        List<Integer> totalList = new ArrayList<>();
+        totalList.addAll(list1);
+        totalList.addAll(list2);
+        totalList = totalList.stream().sorted().collect(Collectors.toList());
+        System.out.println("======sortedList"+totalList.toString());
+        int index = totalList.size()/2;
+        int num = totalList.size()%2;
+        return num == 0 ? ((float)totalList.get(index-1) + (float)totalList.get(index))/2:totalList.get(index);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList(){{
+            add(2);
+            add(5);
+            add(9);
+        }};
+        List<Integer> list2 = new ArrayList(){{
+            add(3);
+            add(6);
+            add(8);
+        }};
+        System.out.println(solution(list1,list2));
+    }
+}
