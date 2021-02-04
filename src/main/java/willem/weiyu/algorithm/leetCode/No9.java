786e327fab8faab09043331e5e4937525d9674c9
weiyu005 <weiyu005@ke.com>
2021-02-04 22:45:38 +0800

    添加leetcode

---
+++ src/main/java/willem/weiyu/algorithm/leetCode/No9.java
@@ -0,0 +1,40 @@
+package willem.weiyu.algorithm.leetCode;
+
+/**
+ * @Author weiyu005@ke.com
+ * @Description No9 给你一个整数x ，如果x是一个回文整数，返回ture；否则，返回false
+ * @Date 2021/2/4 22:18
+ */
+public class No9 {
+    /**
+     * 将数字对折一半，如果为回文数，肯定相等
+     * @param num
+     * @return
+     */
+    public static boolean solution(int num){
+        if (num < 0){
+            return false;
+        }
+        boolean odd = num%2 == 0?false:true;
+        int head = num;
+        int tail = 0;
+        int a = num;
+        int length = 0;
+        while (a > 0){
+            length++;
+            a /=10;
+        }
+        for (int i = 0; i < length/2; i++) {
+            int temp = head%10;
+            head/= 10;
+            tail = tail*10+temp;
+        }
+        if (odd){
+            head/=10;
+        }
+        return head == tail;
+    }
+    public static void main(String[] args) {
+        System.out.println(solution(123321));
+    }
+}
