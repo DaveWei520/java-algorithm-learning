cfe78f7456546da6e4f06f9c1b8acde9cbca8908
weiyu005 <weiyu005@ke.com>
2021-02-06 19:16:45 +0800

    添加leetcode

---
+++ src/main/java/willem/weiyu/algorithm/leetCode/No10.java
@@ -0,0 +1,47 @@
+package willem.weiyu.algorithm.leetCode;
+
+/**
+ * @Author weiyu005@ke.com
+ * @Description No10 给你一个字符串s和一个字符规律p，请你来实现一个支持'.'和'*'的正则表达式匹配
+ * @Date 2021/2/6 18:16
+ */
+public class No10 {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] f = new boolean[m + 1][n + 1];
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p.charAt(j - 1) == '*') {
+                    f[i][j] = f[i][j - 2];
+                    if (matches(s, p, i, j - 1)) {
+                        f[i][j] = f[i][j] || f[i - 1][j];
+                    }
+                } else {
+                    if (matches(s, p, i, j)) {
+                        f[i][j] = f[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return f[m][n];
+    }
+
+    public static boolean matches(String s, String p, int i, int j) {
+        if (i == 0) {
+            return false;
+        }
+        if (p.charAt(j - 1) == '.') {
+            return true;
+        }
+        return s.charAt(i - 1) == p.charAt(j - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "aab";
+        String p = "*c*a*b";
+        System.out.println(isMatch(str,p));
+    }
+}
