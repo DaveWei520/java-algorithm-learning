c0088f2bf7ef4214da66baed1729a95426d3deaa
weiyu005 <weiyu005@ke.com>
2021-02-12 16:40:22 +0800

    添加LeetCode问题

---
+++ src/main/java/willem/weiyu/algorithm/leetCode/No15.java
@@ -0,0 +1,63 @@
+package willem.weiyu.algorithm.leetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author: willem
+ * @create: 2021/02/12 15:59
+ * @description: 给你一个包含n个整数的数组nums，判断nums中是否存在三个元素 a，b，c，使得a+b+c=0？请你找出所有和为0且不重复的三元组
+ */
+public class No15 {
+    public static List<List<Integer>> solution(int[] arr){
+        if (arr == null || arr.length < 3){
+            return null;
+        }
+        int length = arr.length;
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        //一层循环
+        for (int first = 0; first < length; first++) {
+            // 需要和上一次枚举的数不相同
+            if (first>0 && arr[first] == arr[first-1]){
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = length - 1;
+            int target = -arr[first];
+            //二层循环
+            for (int second = first+1; second < length; second++) {
+                // 需要和上一次枚举的数不相同
+                if (second> first+1 && arr[second] == arr[second - 1]){
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && arr[second] + arr[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (arr[second] + arr[third] == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(arr[first]);
+                    list.add(arr[second]);
+                    list.add(arr[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{-1,0,1,2,-1,-4};
+        List<List<Integer>> list = solution(arr);
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> each = list.get(i);
+            each.forEach(e-> System.out.println(e));
+        }
+    }
+}
