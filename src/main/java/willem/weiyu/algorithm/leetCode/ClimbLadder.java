a33f6196f1bfb36ddf3c4ee8728a35cb0a208a83
weiyu005 <weiyu005@ke.com>
2021-02-20 15:59:23 +0800

    添加LeetCode问题

--- src/main/java/willem/weiyu/algorithm/leetCode/ClimbLadder.java
+++ src/main/java/willem/weiyu/algorithm/leetCode/ClimbLadder.java
@@ -43,7 +43,8 @@ public class ClimbLadder {
         }
         int[] dp = new int[ladder+1];
         Arrays.fill(dp, 0);
-        dp[1] = dp[2] = 2;
+        dp[1] = 1;
+        dp[2] = 2;
         for (int i = 3; i <= ladder; i++) {
             dp[i] = dp[i-1] + dp[i - 2];
         }
