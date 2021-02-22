023c4c8924b13eb1c9927c037ba4febcafa145e8
weiyu005 <weiyu005@ke.com>
2021-02-22 14:43:08 +0800

    添加LeetCode问题

--- src/main/java/willem/weiyu/algorithm/leetCode/No1.java
+++ src/main/java/willem/weiyu/algorithm/leetCode/No1.java
@@ -1,7 +1,5 @@
 package willem.weiyu.algorithm.leetCode;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,9 +8,8 @@ import java.util.Map;
 /**
  * @author: willem
  * @create: 2019/08/13 23:57
- * @description: No1 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标
+ * @description: No1 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标
  */
-//@Slf4j
 public class No1 {
 
 
--- src/main/java/willem/weiyu/algorithm/leetCode/No3.java
+++ src/main/java/willem/weiyu/algorithm/leetCode/No3.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 /**
  * @Author willem
- * @Description No3 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
+ * @Description No3 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度
  * @Date 2021/1/19 21:31
  */
 public class No3 {
 
+    /**
+     * 时间复杂度：O(n)，n为字符串的长度
+     * 空间复杂度：O(n)，n为字符串的长度
+     * @param str
+     * @return
+     */
     public static int solution1(String str){
         if (str.length() < 2) {
             return str.length();
@@ -33,7 +39,36 @@ public class No3 {
         return length;
     }
 
+    /**
+     * 时间复杂度：O(n),n为字符串的长度
+     * 空间复杂度：O(1)
+     * @param str
+     * @return
+     */
+    public static int solution2(String str){
+        if (str == null){
+            return 0;
+        }
+        if (str.length() < 2){
+            return str.length();
+        }
+        int maxLen = 0;
+        int start = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int temp = str.indexOf(ch,start);
+            if (i != temp){
+                start = temp+1;
+                maxLen = Math.max(maxLen, i - start+1);
+                continue;
+            }
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
-        System.out.println(solution1("abcabcbb"));
+        String str = "abcabcbb";
+        System.out.println(solution1(str));
+        System.out.println(solution2(str));
     }
 }
